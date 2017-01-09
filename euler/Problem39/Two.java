c796b286406a8185a7a6fa191231703637745f10
jdz1214 <duncan.zahringer@outlook.com>
2017-01-09 08:35:51 -0500

    Problem 39 solved.

--- euler/Problem39/Two.java
+++ euler/Problem39/Two.java
@@ -4,25 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Created by Duncan on 1/8/2017.
+ * Created by Duncan on 1/9/2017.
  */
-public class IntegerRightTriangles {
+public class Two {
 
     public static void main(String[] args) {
-        maximizeP();
+        comboMaker(120);
     }
 
-    public static void maximizeP() {
-        int maxCount = 0;
-        int maxPerimeter = 0;
-        for (int i = 12; i <= 1000; i++) {
-            Set<Set<Integer>> hypoSet = comboMaker(i);
-            if (hypoSet.size() > maxCount) { maxPerimeter = i; maxCount = hypoSet.size(); }
-        }
-        System.out.println("Maximized perimeter with most integral right triangles:\t" + maxPerimeter + "\tnumber of triangles:\t" + maxCount);
-    }
-
-    public static Set<Set<Integer>> comboMaker(int perimeter) {
+    public static void comboMaker(int perimeter) {
         Set<Set<Integer>> hypoSet = new HashSet<>();
         for (int i = 1; i < perimeter; i++) {
             for (int j = 1; j < perimeter; j++) {
@@ -30,9 +20,11 @@ public class IntegerRightTriangles {
                 double b = Math.pow(j,2);
                 double hypoSquared = a + b;
                 double hypo = Math.sqrt(hypoSquared);
+                //System.out.println("a:\t" + i + "\tb:\t" + j + "\thypo\t" + hypo);
                 if (hypo % 1.0 == 0.0) {
+                    //System.out.println("Even hypotenuse:\t" + hypo);
                     if (hypo + i + j == perimeter) {
-                        //System.out.println("Integral right triangle: a:\t" + i + "\tb:\t" + j + "\thypo\t" + hypo);
+                        System.out.println("Integral right triangle: a:\t" + i + "\tb:\t" + j + "\thypo\t" + hypo);
                         Set<Integer> set = new HashSet<>();
                         set.add(i);
                         set.add(j);
@@ -41,6 +33,8 @@ public class IntegerRightTriangles {
                 }
             }
         }
-        return hypoSet;
+        System.out.println("Integral set size:\t" + hypoSet.size());
+        hypoSet.forEach(System.out::println);
+
     }
 }
---
+++ src/HalfLife.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Duncan on 1/9/2017.
+ */
+public class HalfLife {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
