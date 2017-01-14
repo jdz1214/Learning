5472b73aa007ba2a7bb2afca043fe8d93a4e4843
jdz1214 <duncan.zahringer@outlook.com>
2017-01-13 19:22:42 -0500

    Problem 47 Solved.

--- euler/Problem47/DistinctPrimeFactors.java
+++ euler/Problem47/DistinctPrimeFactors.java
@@ -4,7 +4,6 @@ import Utils.MathUtils;
 
 import java.util.Arrays;
 import java.util.OptionalLong;
-import java.util.stream.LongStream;
 
 /**
  * Created by Duncan on 1/13/2017.
@@ -12,19 +11,31 @@ import java.util.stream.LongStream;
 public class DistinctPrimeFactors {
 
     public static void main(String[] args) {
-        //hasDistinctPrimeFactors(644, 3);
-        long count = 644;
         long digits = 4;
-        long[] consecutivePrimes = hasDistinctPrimeFactors(count, digits, new long[]{});
-
-        System.out.println("Solution found:\t" + consecutivePrimes[0]);
-        System.out.println("First number in sequence:\t" + count);
-
+        long count = 0;
+        long[] one = new long[]{};
+        long[] two = new long[]{};
+        long[] three = new long[]{};
+        long[] four = new long[]{};
+        boolean found = false;
+        for (long i = 35000; i <= 10000000; i++) {
+            one = hasDistinctPrimeFactors(i, digits, new long[]{});
+            two = hasDistinctPrimeFactors(i + 1, digits, new long[]{});
+            three = hasDistinctPrimeFactors(i + 2, digits, new long[]{});
+            four = hasDistinctPrimeFactors(i + 3, digits, new long[]{});
+            if (one.length >= 1 && two.length >= 1 && three.length >= 1 && four.length >= 1) { count = i; found = true; break; }
+        }
+        if (found) {
+            System.out.println("Solution found:\t" + count);
+            Arrays.stream(one).forEach(x -> System.out.print(x + "\t")); System.out.println("");
+            Arrays.stream(two).forEach(x -> System.out.print(x + "\t")); System.out.println("");
+            Arrays.stream(three).forEach(x -> System.out.print(x + "\t")); System.out.println("");
+            Arrays.stream(four).forEach(x -> System.out.print(x + "\t")); System.out.println("");
+        }
     }
 
     public static long[] hasDistinctPrimeFactors(long n, long distinctPrimeFactors, long[] array) {
-        System.out.println("n:\t" + n);
-        if (array.length == 4) { return array; }
+        //System.out.println("n:\t" + n);
         long[] arr = MathUtils.getPrimeFactors(n);
         if (arr.length == distinctPrimeFactors) {
             long z = Arrays.stream(arr).reduce((x, y) -> x * y).getAsLong(); // Prime Factor Multiples
@@ -33,40 +44,14 @@ public class DistinctPrimeFactors {
                 if (doubleDigit.isPresent()) {
                     //Arrays.stream(arr).forEach(s -> System.out.print(s + "\t"));
                     //System.out.println("\t==\t" + n + "\t(using factor " + doubleDigit.getAsLong() + " twice.)");
-                    long[] newArray = LongStream.concat(Arrays.stream(array), LongStream.of(n)).toArray();
-                    return hasDistinctPrimeFactors(n + 1, distinctPrimeFactors, newArray);
+                    return arr;
                 }
             } else if (z == n) {
                 //Arrays.stream(arr).forEach(r -> System.out.print(r + "\t"));
                 //System.out.println("\t==\t" + n);
-                long[] newArray = LongStream.concat(Arrays.stream(array), LongStream.of(n)).toArray();
-                return hasDistinctPrimeFactors(n + 1, distinctPrimeFactors, newArray);
+                return arr;
             }
         }
-        return hasDistinctPrimeFactors(n + 1, distinctPrimeFactors, new long[]{});
+        return new long[]{};
     }
 }
-
-
-//    public static boolean hasDistinctPrimeFactors(long n) {
-//        long[] arr = MathUtils.getPrimeFactors(n);
-//        Predicate<Long> d1 = d -> {
-//            if (d < n) {
-//                Arrays.stream(arr).filter(a -> d * a == n).findFirst().ifPresent(b -> {
-//                    Arrays.stream(arr).forEach(s -> System.out.print(s + "\t"));
-//                    System.out.print("\tUse factor " + b + " twice.");
-//                });
-//            }
-//            return false;
-//        };
-//
-//
-//
-//
-//
-//        Arrays.stream(arr).reduce((x , y) -> x * y).ifPresent(z -> {
-//            if (z < n) {
-//                return Arrays.stream(arr).filter(a -> z * a == n).allMatch(d1).findFirst();
-//            } else if (z == n) { Arrays.stream(arr).forEach(r -> System.out.print(r + "\t")); System.out.print("\t == " + n); }
-//        });
-//    }
